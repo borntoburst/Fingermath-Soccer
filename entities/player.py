fe1d4d25315d23557062e8ac20d2dbcf25a14d7c
borntoburst <minhthangnguyen2109@gmail.com>
2026-07-15 19:47:43 +0700

    Create player.py

---
+++ entities/player.py
@@ -0,0 +1,20 @@
+@dataclass
+class Player:
+
+    id: int
+
+    name: str
+
+    team: TeamColor
+
+    role: Role
+
+    x: float
+
+    y: float
+
+    speed: float
+
+    stamina: float
+
+    has_ball: bool
